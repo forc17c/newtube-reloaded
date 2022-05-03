69c7921a66f95ccc80706080766ea469d92dd9b5
Changhyeon <forc17c@gmail.com>
2022-05-03 13:29:08 +0900

    #3.3 Responses

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 import express from "express"; // const express = require("express"); 와 같다.
+import req from "express/lib/request";
 
 const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("Somebody is trying to go home.");
-
+const handleHome = (req, res) => {
+  return res.send("I still love you.");
+};
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
+};
 app.get("/", handleHome); // app.get("/", () => console.log("Somebody is trying to go home")); 로 쓸 수 있다.
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening); // app.listen(4000, () => console.log("Server listening on port 4000")); 로 쓸 수 있다.
+app.listen(PORT, handleListening); // app.listen(4000, () => console.log(`Server listening on port http://localhost:${PORT}`)); 로 쓸 수 있다.
