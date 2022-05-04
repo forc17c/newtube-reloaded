54d94a93aef4d4675dfcf096ef2b570b1f7626ef
Changhyeon <forc17c@gmail.com>
2022-05-04 14:21:09 +0900

    #3.6 Middlewares part Two

--- src/server.js
+++ src/server.js
@@ -4,19 +4,38 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
 };
 
-const handleHome = (req, res, next) => {
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
+  next();
+};
+
+const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
+
 const handleLogin = (req, res) => {
   return res.send({ message: "Login here." });
 };
-app.get("/", gossipMiddleware, handleHome); // app.get("/", () => console.log("Somebody is trying to go home")); 로 쓸 수 있다.
+
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+app.use(logger);
+app.use(privateMiddleware); // 모든 app에 대하여 middleware를 적용
+
+app.get("/", handleHome); // app.get("/", () => console.log("Somebody is trying to go home")); 로 쓸 수 있다.
 app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
