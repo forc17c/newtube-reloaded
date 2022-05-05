1b585c58cf133e6bd647a449826047c4bca038c0
Changhyeon <forc17c@gmail.com>
2022-05-05 17:42:50 +0900

    #3.11 External Middlewares

--- src/server.js
+++ src/server.js
@@ -1,28 +1,10 @@
 import express from "express"; // const express = require("express"); 와 같다.
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url} ${req.protocol} ${req.path}`);
-  if (req.protocol === "https") {
-    console.log("Secure");
-  } else {
-    console.log("Insecure");
-  }
-  console.dir(req.protocol);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
   const year = new Date().getFullYear();
@@ -40,8 +22,7 @@ const handleProtected = (req, res) => {
   return res.send("Welcome to the private lounge.");
 };
 
-app.use(logger);
-app.use(privateMiddleware); // 모든 app에 대하여 middleware를 적용
+app.use(logger); // app.use : 모든 app에 대하여 middleware를 적용
 
 app.get("/", handleHome); // app.get("/", () => console.log("Somebody is trying to go home")); 로 쓸 수 있다.
 app.get("/login", handleLogin);
