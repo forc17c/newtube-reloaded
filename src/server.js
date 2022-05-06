7162f38a859b3ec46e092aedf4e85fe9fbc456b5
Changhyeon <forc17c@gmail.com>
2022-05-06 13:58:07 +0900

    #4.1 Making Our Routers

--- src/server.js
+++ src/server.js
@@ -4,30 +4,36 @@ import morgan from "morgan";
 const PORT = 4000;
 
 const app = express();
-const logger = morgan("dev");
-
-const handleHome = (req, res) => {
-  const year = new Date().getFullYear();
-  const month = new Date().getMonth() + 1;
-  const date = new Date().getDate();
-  console.log("Time:", year + "." + month + "." + date);
-  return res.send("I love middlewares");
-};
-
-const handleLogin = (req, res) => {
-  return res.send({ message: "Login here." });
-};
-
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge.");
-};
+const logger = morgan("dev"); // middleware의 하나
 
 app.use(logger); // app.use : 모든 app에 대하여 middleware를 적용
 
-app.get("/", handleHome); // app.get("/", () => console.log("Somebody is trying to go home")); 로 쓸 수 있다.
-app.get("/login", handleLogin);
-app.get("/protected", handleProtected);
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
+// app.get("/", () => console.log("Somebody is trying to go home")); 로 쓸 수 있다.
+
 // middleware는 위에서부터 아래로 적용
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
