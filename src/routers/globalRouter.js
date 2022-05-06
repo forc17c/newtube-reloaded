d75dd753d50205f45ed177e3f453c8f154372a7d
Changhyeon <forc17c@gmail.com>
2022-05-06 15:38:29 +0900

    #4.6 Planning Routes

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { join } from "../controllers/userController"; //각각의 export로 불러온 파일은 export한 이름으로 import해야한다.
-import { trending } from "../controllers/videoController";
+import { join, login } from "../controllers/userController"; //각각의 export로 불러온 파일은 export한 이름으로 import해야한다.
+import { trending, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
