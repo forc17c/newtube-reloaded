f813be9e58ea9ea2fecd78247b9393e4e737bde0
Changhyeon <forc17c@gmail.com>
2022-05-06 14:23:25 +0900

    #4.2 Cleaning the Code

--- src/server.js
+++ src/server.js
@@ -1,34 +1,18 @@
 import express from "express"; // const express = require("express"); 와 같다.
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev"); // middleware의 하나
-
 app.use(logger); // app.use : 모든 app에 대하여 middleware를 적용
 
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
-
 app.use("/", globalRouter);
-app.use("/users", userRouter);
 app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 // app.get("/", () => console.log("Somebody is trying to go home")); 로 쓸 수 있다.
 
