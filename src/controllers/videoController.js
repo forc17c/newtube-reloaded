ed722a602827c5019a009cdeae211a59b8b5beea
Changhyeon <forc17c@gmail.com>
2022-05-09 15:03:18 +0900

    #5.4 Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,6 @@
-export const trending = (req, res) => res.render("home"); // 작명한 pug의 view로 naming한다. ex)'home.pug -> res.resnder("home")'
-export const see = (req, res) => res.render("watch");
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", potato: "tomato" }); // 작명한 pug의 view로 naming한다. ex)'home.pug -> res.resnder("home")'
+export const see = (req, res) => res.render("watch"); // pug로 변수를 보낼 수 있다 ex){pageTitle: ""}
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
