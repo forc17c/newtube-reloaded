b02a0fd8e7eff24b34167d74dc827a24bf8a916a
Changhyeon <forc17c@gmail.com>
2022-05-10 13:54:46 +0900

    #5.7 Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,11 @@
+const fakeUser = {
+  username: "Nicolas",
+  loggedIn: false,
+};
+
 export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", potato: "tomato" }); // 작명한 pug의 view로 naming한다. ex)'home.pug -> res.resnder("home")'
+  res.render("home", { pageTitle: "Home", fakeUser }); // 작명한 pug의 view로 naming한다. ex)'home.pug -> res.resnder("home")'
+// fakeUser: fakeUser는 ES6에서 fakeUser와 같다. ex) { pageTitle: "Home", fakeUser } = { pageTitle: "Home", fakeUser: fakeUser }
 export const see = (req, res) => res.render("watch"); // pug로 변수를 보낼 수 있다 ex){pageTitle: ""}
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
