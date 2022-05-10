d16904a946bead3549b74b347653c37d320aea88
Changhyeon <forc17c@gmail.com>
2022-05-10 14:12:47 +0900

    #5.8 Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,11 @@
-const fakeUser = {
-  username: "Nicolas",
-  loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser }); // 작명한 pug의 view로 naming한다. ex)'home.pug -> res.resnder("home")'
+// 작명한 pug의 view로 naming한다. ex)'home.pug -> res.resnder("home")'
 // fakeUser: fakeUser는 ES6에서 fakeUser와 같다. ex) { pageTitle: "Home", fakeUser } = { pageTitle: "Home", fakeUser: fakeUser }
-export const see = (req, res) => res.render("watch"); // pug로 변수를 보낼 수 있다 ex){pageTitle: ""}
+// pug로 변수를 보낼 수 있다 ex){pageTitle: ""}
+export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
