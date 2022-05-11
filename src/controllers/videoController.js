a02e616036ab7fd71218d661b9ef305ccf581d1d
Changhyeon <forc17c@gmail.com>
2022-05-11 14:02:41 +0900

    #6.1 Array Database part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ let videos = [
     rating: 5,
     comments: 2,
     createdAt: "2 minutes ago",
-    views: 59,
+    views: 1,
     id: 1,
   },
   {
@@ -30,10 +30,10 @@ export const trending = (req, res) => {
 // 작명한 pug의 view로 naming한다. ex)'home.pug -> res.resnder("home")'
 // fakeUser: fakeUser는 ES6에서 fakeUser와 같다. ex) { pageTitle: "Home", fakeUser } = { pageTitle: "Home", fakeUser: fakeUser }
 // pug로 변수를 보낼 수 있다 ex){pageTitle: ""}
-export const see = (req, res) => {
+export const watch = (req, res) => {
   const { id } = req.params; // ES6에서 const id = req.params.id; 를 const { id } = req.params; 로 쓸 수 있다.
   const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
