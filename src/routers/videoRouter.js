fba541a63aab512da7ab5f11736074fb5481df53
Changhyeon <forc17c@gmail.com>
2022-05-11 15:51:27 +0900

    #6.4 Recap

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,7 +3,8 @@ import { watch, getEdit, postEdit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
+videoRouter.get("/:id(\\d+)", watch); // -> videoRouter.route("/:id(\\d+)").get(watch); 로 쓸 수 있다.
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+// get은 정보를 받는다.(form을 화면에 보여준다) post는 정보를 보낸다.(변경사항을 저장해준다)
 
 export default videoRouter;
